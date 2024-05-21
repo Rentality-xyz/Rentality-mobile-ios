92b6a770840011e9d5e98dbfcb35544a9565202b
danyloday <danil.vladyka1@gmail.com>
2024-05-21 18:47:18 +0300

    Initial Commit

---
+++ Rentality/RentalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentalityApp.swift
+//  Rentality
+//
+//  Created by Danylo Vladyka on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RentalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
