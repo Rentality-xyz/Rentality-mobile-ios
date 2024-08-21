0e3eb9904422481f160be87111679ca24e56fb8e
danyloday <danil.vladyka1@gmail.com>
2024-08-21 14:17:52 +0300

    work

---
+++ Rentality/WebView.swift
@@ -0,0 +1,18 @@
+//
+//  WebView.swift
+//  Rentality
+//
+//  Created by Danylo Vladyka on 28.05.2024.
+//
+
+import SwiftUI
+
+struct WebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebView()
+}
